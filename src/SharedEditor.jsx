cc8e056fd2b549ee436b05c373d3a49e3183412d
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2020-03-23 20:52:18 +0530

    Added basic collaborative capabilities

--- src/SharedEditor.jsx
+++ src/SharedEditor.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from "react";
-import Editor from "@monaco-editor/react";
+import { ControlledEditor } from "@monaco-editor/react";
+import socketIOClient from "socket.io-client";
 import "./SharedEditor.css";
 
 class SharedEditor extends Component {
+  state = {
+    value: "",
+    remoteChange: false
+  };
+
+  componentDidMount() {
+    let socket = socketIOClient("http://192.168.1.130:9000");
+
+    socket.on("remote editor change", value => {
+      let currentState = {};
+      Object.assign(currentState, this.state);
+      currentState.value = value;
+      currentState.remoteChange = true;
+      this.setState(currentState);
+    });
+
+    let currentState = {};
+    Object.assign(currentState, this.state);
+    this.setState({ ...currentState, socket });
+  }
+
+  handleEditorChange = (ev, value) => {
+    if (!this.state.remoteChange) {
+      this.state.socket.emit("editor change", value);
+    } else {
+      let currentState = {};
+      Object.assign(currentState, this.state);
+      currentState.remoteChange = false;
+      this.setState(currentState);
+    }
+
+    if (value.includes("Surya")) {
+      this.setState({ value: "SURYA" });
+    } else {
+      this.setState({ value: value });
+    }
+  };
+
   render() {
     return (
       <div className="shared-editor">
-        <Editor height="90vh" language="javascript" theme="dark" />
+        <ControlledEditor
+          language="javascript"
+          value={this.state.value}
+          onChange={(ev, value) => {
+            this.handleEditorChange(ev, value);
+          }}
+          theme="dark"
+        />
       </div>
     );
   }
