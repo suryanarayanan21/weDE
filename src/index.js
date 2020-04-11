cdab4a6618d74c8f8304e78f0bfddcf754d086e5
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2020-04-11 18:34:10 +0530

    Saved dependencies

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import ProjectList from "./Projects";
-import SharedEditor from "./sharedEditor/SharedEditor";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import reducer from "./store/reducers/reducer";
+import App from "./App";
 
-let store = createStore(reducer);
-=======
->>>>>>> 09b3b16147cc770a5e8ad51a522f6850e5ee2a18
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from "./serviceWorker";
-import App from './App';
-
-ReactDOM.render(<App />,document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
 serviceWorker.unregister();
-
